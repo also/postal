66c840de766c7585ae5fb81128268e389ed0654e
Ryan Berdeen <ryan@ryanberdeen.com>
2008-10-07 07:41:12 -0700

    added lgpl

--- src/main/java/com/ryanberdeen/postal/message/IncomingResponseMessage.java
+++ src/main/java/com/ryanberdeen/postal/message/IncomingResponseMessage.java
@@ -1,4 +1,21 @@
-/* $Id$ */
+/*
+ * Copyright 2008 Ryan Berdeen.
+ *
+ * This file is part of Postal.
+ *
+ * Postal is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Postal is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Postal.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package com.ryanberdeen.postal.message;
 
